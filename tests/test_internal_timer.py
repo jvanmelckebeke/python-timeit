ea982718c9f0e67d7084ad79260e9f3d0f823477
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2023-09-23 10:42:39 +0200

    fixed test internal timer ordering

--- tests/test_internal_timer.py
+++ tests/test_internal_timer.py
@@ -7,7 +7,7 @@ from timeitpoj.timer.internal_timer import InternalTimer
 class InternalTimerTests(unittest.TestCase):
     def test_internal_time_starts_at_zero(self):
         timer = InternalTimer()
-        self.assertEqual(timer.internal_time, 0)
+        self.assertEqual(0, timer.internal_time)
 
     def test_context_manager_tracks_elapsed_time(self):
         with InternalTimer() as timer:
@@ -22,8 +22,8 @@ class InternalTimerTests(unittest.TestCase):
         with InternalTimer() as timer2:
             time.sleep(0.2)
 
-        self.assertAlmostEqual(timer1.internal_time, 0.1, places=1)
-        self.assertAlmostEqual(timer2.internal_time, 0.2, places=1)
+        self.assertAlmostEqual(0.1, timer1.internal_time, places=1)
+        self.assertAlmostEqual(0.2, timer2.internal_time, places=1)
 
     def test_nested_context_managers_accumulate_elapsed_time(self):
         with InternalTimer() as timer1:
@@ -32,8 +32,8 @@ class InternalTimerTests(unittest.TestCase):
             with InternalTimer() as timer2:
                 time.sleep(0.2)
 
-        self.assertAlmostEqual(timer1.internal_time, 0.3, places=1)
-        self.assertAlmostEqual(timer2.internal_time, 0.2, places=1)
+        self.assertAlmostEqual(0.3, timer1.internal_time, places=1)
+        self.assertAlmostEqual(0.2, timer2.internal_time, places=1)
 
 
 if __name__ == '__main__':
