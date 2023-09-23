dfe21ab5b4c2ac3ca755ffa9898332fef0afc66c
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2023-09-23 10:14:47 +0200

    fixed test misc ordering

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -5,28 +5,28 @@ from timeitpoj.utils.misc import reformat_units, time_to_str, format_percentage,
 class MiscTestCase(unittest.TestCase):
     def test_reformat_units(self):
         # Test reformatting units
-        self.assertEqual(reformat_units(0.005), (5.0, 'milliseconds'))
-        self.assertEqual(reformat_units(0.00002), (20.0, 'microseconds'))
-        self.assertEqual(reformat_units(0.0000003), (0.3, 'microseconds'))
+        self.assertEqual((5.0, 'milliseconds'), reformat_units(0.005))
+        self.assertEqual((20.0, 'microseconds'), reformat_units(0.00002))
+        self.assertEqual((0.3, 'microseconds'), reformat_units(0.0000003))
 
         # Test start_unit parameter
-        self.assertEqual(reformat_units(100, start_unit='milliseconds'), (100.0, 'milliseconds'))
+        self.assertEqual((100.0, 'milliseconds'), reformat_units(100, start_unit='milliseconds'))
 
     def test_time_to_str(self):
         # Test time formatting
-        self.assertEqual(time_to_str(3.14159), '3.142 seconds')
-        self.assertEqual(time_to_str(0.005, unit='milliseconds'), '5.000 microseconds')
-        self.assertEqual(time_to_str(0.00002, unit='microseconds'), '0.020 nanoseconds')
+        self.assertEqual('3.142 seconds', time_to_str(3.14159))
+        self.assertEqual('5.000 microseconds', time_to_str(0.005, unit='milliseconds'))
+        self.assertEqual('0.020 nanoseconds', time_to_str(0.00002, unit='microseconds'))
 
         # Test minute formatting
-        self.assertEqual(time_to_str(61), '1 minutes 1.000 seconds')
-        self.assertEqual(time_to_str(120), '2 minutes')
+        self.assertEqual('1 minutes 1.000 seconds', time_to_str(61))
+        self.assertEqual('2 minutes', time_to_str(120))
 
     def test_format_percentage(self):
         # Test percentage formatting
-        self.assertEqual(format_percentage(0.25), '[25.00%]')
-        self.assertEqual(format_percentage(0.75, include_brackets=False), '75.00%')
-        self.assertEqual(format_percentage(1.0), '[100.00%]')
+        self.assertEqual('[25.00%]', format_percentage(0.25))
+        self.assertEqual('75.00%', format_percentage(0.75, include_brackets=False))
+        self.assertEqual('[100.00%]', format_percentage(1.0))
 
     def test_random_task_name(self):
         # Test generation of random task names
